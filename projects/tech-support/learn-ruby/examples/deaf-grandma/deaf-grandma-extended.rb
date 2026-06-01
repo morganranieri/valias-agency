33734fcc8517a53a89a69aa8e551b6ed5d197791
Morgan Ranieri <morgan.ranieri@gmail.com>
2026-06-01 19:58:54 +1000

    update

--- projects/tech-support/learn-ruby/examples/deaf-grandma/deaf-grandma-extended.rb
+++ projects/tech-support/learn-ruby/examples/deaf-grandma/deaf-grandma-extended.rb
@@ -1,16 +1,27 @@
-farewell = ["BYE","BYE","BYE"]
-last3ThingsISaid = ["Hi","Hi","Hi"]
-something = "hello"
+# when the last two things I say to Grandma are "BYE", program should exit. 
 
-while last3ThingsISaid != farewell
+farewell = ["BYE","BYE"]
+thingsISaid = ["", "hmm"]
+something = "hello"
+thingBefore = thingsISaid.at(-2) 
+lastThing = thingsISaid.last
+last2ThingsISaid = [thingBefore, lastThing]
+
+# how many things have I said, thingsISaid.count
+# grab the last thing from the array 
+# grab the thing before that 
+
+while last2ThingsISaid != farewell
   puts "Computer: say something to Grandma.."
   something = gets.chomp
+  thingsISaid << something
   somethingQuiet = something.downcase
     if something == somethingQuiet
       puts "Grandma: HUH?!  SPEAK UP, SONNY!"
     else
       puts "Grandma: NO, NOT SINCE " + (1930 + rand(20)).to_s + "!"
     end
+  
 end
 
 
