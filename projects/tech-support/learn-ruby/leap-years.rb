92e21ea7d9da714dc380180411394b8fb0222078
Morgan Ranieri <morgan.ranieri@gmail.com>
2026-07-27 12:38:38 +1000

    update

---
+++ projects/tech-support/learn-ruby/leap-years.rb
@@ -0,0 +1,4 @@
+# ask for a starting year 
+# ask for an ending year 
+# puts all of the leap years between them, including them, if they are leap years
+# leap years are all years divisible by 4, except years divisible by 100 are not leap years unless they are divisible by 400. 
